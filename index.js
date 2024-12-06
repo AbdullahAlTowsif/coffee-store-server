bd1ef3082ba572fb7e50de80378ff4d1da27602e
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2024-12-06 19:49:23 +0600

    vercel deploy done

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         const coffeeCollection = client.db('coffeeDB').collection('coffee');
         const userCollection = client.db('coffeeDB').collection('users');
 
@@ -77,10 +77,36 @@ async function run() {
         })
 
         // user related apis
+
+        app.get('/users', async(req, res)=> {
+            const cursor = userCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
         app.post('/users', async(req, res)=> {
             const newUser = req.body;
             console.log('creating new user: ', newUser);
             const result = await userCollection.insertOne(newUser);
+            res.send(result);
+        })
+
+        app.patch('/users', async(req, res)=> {
+            const email = req.body.email;
+            const filter = {email}
+            const updatedDoc = {
+                $set: {
+                    lastSignInTime: req.body?.lastSignInTime
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc);
+            res.send(result);
+        })
+
+        app.delete('/users/:id', async(req, res)=> {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await userCollection.deleteOne(query);
             res.send(result);
         })
 
